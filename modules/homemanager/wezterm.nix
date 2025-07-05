50753a80801efa3b70c8d816eed9c8de1364ad26
Benedict-Hyland <benedicthyland@gmail.com>
2025-07-05 21:37:26 +0100

    Fixing Wezterm mistakes

--- modules/homemanager/wezterm.nix
+++ modules/homemanager/wezterm.nix
@@ -1,17 +1,12 @@
+# home-manager/users/hiddenb/wezterm.nix
 { config, lib, pkgs, ... }:
 
 let
   cfg = config.modules.homemanager.wezterm;
-in
-{
-  options.modules.homemanager.wezterm = {
-    enable = lib.mkEnableOption "wezterm terminal emulator";
-  };
+in {
+  options.modules.homemanager.wezterm.enable = lib.mkEnableOption "Enable wezterm terminal";
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ wezterm ];
-
-    # Optional: Set as default terminal in systemd (for apps that rely on $TERM_PROGRAM)
-    # environment.variables.TERM_PROGRAM = "wezterm";
+    home.packages = with pkgs; [ wezterm ];
   };
 }
